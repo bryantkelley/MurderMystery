00d22a8dba4396364dc947556cf0112fd71b7f8e
Bryant Kelley <bryantkelley14@icloud.com>
2020-07-20 22:05:34 -0500

    Make a fun murder mystery game with names of friends.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Badge, Button, Col, Container, Row } from 'react-bootstrap';
 
 function App() {
+  const [card, setCard] = useState('_____');
+  const [dice, setDice] = useState('_____');
+  const [spinner, setSpinner] = useState('_____');
+
+  const people = ['robbie', 'jess', 'ali', 'marissa', 'mel', 'katie', 'zap', 'tact'];
+  const weapons = ['candlestick', 'revolver', 'rope', 'wrench', 'lead pipe', 'dagger'];
+  const locations = [
+    'study',
+    'library',
+    'conservatory',
+    'hall',
+    'kitchen',
+    'ballroom',
+    'dining room',
+    'lounge',
+    'billiard room',
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="App display-flex justify-content-center align-items-center">
+      <Row className="pt-5">
+        <Col>
+          <Button
+            variant="primary"
+            onClick={() => {
+              setCard(people[Math.floor(Math.random() * people.length)]);
+            }}
+          >Pick A Card</Button>
+        </Col>
+        <Col>
+          <Button
+            variant="success"
+            onClick={() => {
+              setDice(locations[Math.floor(Math.random() * locations.length)]);
+            }}
+          >Roll Dice</Button>
+        </Col>
+        <Col>
+          <Button
+            variant="warning"
+            onClick={() => {
+              setSpinner(weapons[Math.floor(Math.random() * weapons.length)]);
+            }}
+          >Spin Wheel</Button>
+        </Col>
+        <Col>
+          <Button
+            variant="danger"
+            onClick={() => {
+              setCard('_____');
+              setDice('_____');
+              setSpinner('_____');
+            }}
+          >Reset Game</Button>
+        </Col>
+      </Row>
+      <Row className="pt-5">
+        <Col>
+          <h1>It was <Badge variant="primary">{card}</Badge>,</h1>
+          <h1>in the <Badge variant="success">{dice}</Badge>,</h1>
+          <h1>with the <Badge variant="warning">{spinner}</Badge>.</h1>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
